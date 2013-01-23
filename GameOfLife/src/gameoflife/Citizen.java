6962ebb2ecc982ea248ae93cbc66ff3cfaecd67b
Sam Sobell <sam.sobell@gmail.com>
2013-01-23 11:06:48 -0800

    Initial game
    Here are the initial files for the game

---
+++ GameOfLife/src/gameoflife/Citizen.java
@@ -0,0 +1,11 @@
+
+package gameoflife;
+
+public class Citizen {
+    boolean alive;
+    int positionx = ;
+    int positiony = ;
+    public static void Update () {
+        
+    }
+}
---
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameoflife;
+
+/**
+ *
+ * @author sam
+ */
+public class GameOfLife {
+    
+    public static void main(String[] args) {
+        Citizen[][] gameboard = new Citizen[10][10];
+        
+    }
+}
